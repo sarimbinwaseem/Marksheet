edab56eeb99914d7502b8d6ce7c62de4d42606d6
sarimbinwaseem <42667306+sarimbinwaseem@users.noreply.github.com>
2019-06-29 11:33:41 +0500

    Update Marksheet.py

--- Marksheet.py
+++ Marksheet.py
@@ -1,3 +1,5 @@
+import csv
+
 maths = int(input("Enter Mathamatics marks: "))
 phy = int(input("Enter Physics marks: "))
 comp = int(input("Enter Computer marks: "))
@@ -10,13 +12,33 @@ print("============================================")
 print("Your percentage is:", per)
 print("--------------------------------------------")
 if per < 60:
-	print("You are fail..!!")
+	grade = "You are fail..!!"
 elif per <= 70:
-	print("Grade: \"B\"")
+	grade = "\"B\""
 elif per < 80:
-	print("Grade: \"A\"")
+	grade = "\"A\""
 elif per < 90:
-	print("Grade: \"A+\"")
+	grade = "\"A+\""
 print("")
 print("")
 print("End of Result..")
+
+marksheet = open("marksheet.txt","w") 
+ 
+marksheet.write("Marks of Mathematics:")
+marksheet.write(str(maths) + "\n") 
+marksheet.write("Marks of Physics:") 
+marksheet.write(str(phy) + "\n")
+marksheet.write("Marks of Computer:") 
+marksheet.write(str(comp) + "\n")
+marksheet.write("Marks of Urdu:") 
+marksheet.write(str(urdu) + "\n")
+marksheet.write("Marks of English:")
+marksheet.write(str(eng) + "\n \n")
+marksheet.write("Percentage:")
+marksheet.write(str(per) + "%"+ "\n \n")
+marksheet.write("Grade:")
+marksheet.write(str(grade) + "\n")
+
+marksheet.close() 
+
